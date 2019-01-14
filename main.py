0de147a8a964ac0079f6444e1f986c60c446bfa7
ijemmao <ijemmao>
2019-01-13 21:55:48 -0500

    able to evaluate and predict

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ labels = np.random.random((1000, 10))
 
 model.fit(data, labels, epochs=10, batch_size=32)
 
+dataset = tf.data.Dataset.from_tensor_slices((data, labels))
+dataset = dataset.batch(32)
+dataset = dataset.repeat()
+
+model.fit(dataset, epochs=10, steps_per_epoch=20)
+
+model.evaluate(data, labels, batch_size=32)
+model.evaluate(dataset, steps=30)
+
+result = model.predict(data, batch_size=32)
+print(result.shape)
