f9b645a90112f8e52aa4895c4315d9258a33bf7c
arinehouse <arinehouse@yahoo.com>
2019-01-14 18:32:32 -0500

    Backup

--- tensorflow/prepare_image.py
+++ tensorflow/prepare_image.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import os
 
 def prepare_image(img, size=(64,64)):
     """
@@ -15,8 +16,40 @@ def prepare_image(img, size=(64,64)):
     out = im.convert("L").resize(size)
     out.thumbnail(size, Image.ANTIALIAS)
 
-    # get the numeric data from the image
-    data = np.array(list(out.getdata()))
+    return out
 
-    # divide all the data by 255
-    return np.divide(data, 255)
+    # # get the numeric data from the image
+    # data = np.array(list(out.getdata()))
+
+    # # divide all the data by 255
+    # return np.divide(data, 255)
+
+def create_dataset(path, size=(64,64)):
+    """
+    Takes a folder of images and resaves the images as processed data
+
+    path (string): the path to the dataset
+    size (tuple of ints): the 2D size of the intended image
+    """
+    # preprocess the image data and compile it for training
+    train_images = np.empty((0, size[0]*size[1]), float)
+
+    for dirpath, _, filenames in os.walk(path):
+        print(filenames)
+        for f in filenames:
+            path = "{}/{}".format(dirpath, f)
+            # ignore hidden files
+            if f[0] == '.':
+                continue
+            try:
+                image_data = prepare_image(path)
+                image_data.save(f)
+                # train_images = np.append(train_images, np.array([image_data]), axis=0)
+            except OSError:
+                os.remove(path)
+
+
+# for as many train_images as there are, set all of their labels to be 1
+# train_labels = np.ones(train_images.shape[0])
+
+# return (train_images, train_labels)
--- tensorflow/train_model.py
+++ tensorflow/train_model.py
@@ -5,29 +5,7 @@ import os
 
 from prepare_image import prepare_image
 
-def compile_data(path, size=(64,64)):
-    # preprocess the image data and compile it for training
-    train_images = np.empty((0, size[0]*size[1]), float)
-
-    for dirpath, dirnames, filenames in os.walk(path):
-        for f in filenames:
-            # ignore hidden files
-            if f[0] == '.':
-                continue
-            try:
-                image_data = prepare_image("{}/{}".format(dirpath, f))
-                train_images = np.append(train_images, np.array([image_data]), axis=0)
-            except OSError:
-                # do nothing
-                continue
-
-    # for as many train_images as there are, set all of their labels to be 1
-    train_labels = np.ones(train_images.shape[0])
-
-    return (train_images, train_labels)
-
-
-def train_model(train_images, train_labels)
+def train_model(train_images, train_labels):
     # build the model architecture
     model = keras.Sequential([
         keras.layers.Dense(128, activation=tf.nn.relu),
