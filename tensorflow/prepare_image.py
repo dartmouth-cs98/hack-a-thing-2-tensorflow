911b21d2a8405ad06c3a5279d6e08c57f8f1aa95
arinehouse <arinehouse@yahoo.com>
2019-01-14 19:34:17 -0500

    Code refactoring

--- tensorflow/prepare_image.py
+++ tensorflow/prepare_image.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import os
+import sys
 
 def prepare_image(img, size=(64,64)):
     """
@@ -18,12 +19,6 @@ def prepare_image(img, size=(64,64)):
 
     return out
 
-    # # get the numeric data from the image
-    # data = np.array(list(out.getdata()))
-
-    # # divide all the data by 255
-    # return np.divide(data, 255)
-
 def create_dataset(path, size=(64,64)):
     """
     Takes a folder of images and resaves the images as processed data
@@ -31,25 +26,24 @@ def create_dataset(path, size=(64,64)):
     path (string): the path to the dataset
     size (tuple of ints): the 2D size of the intended image
     """
-    # preprocess the image data and compile it for training
-    train_images = np.empty((0, size[0]*size[1]), float)
+    path = os.path.abspath(path)
+    prepared_path = path+"_prepared"
+    
+    try:
+        os.mkdir(prepared_path)
+    except:
+        pass
 
-    for dirpath, _, filenames in os.walk(path):
-        print(filenames)
-        for f in filenames:
-            path = "{}/{}".format(dirpath, f)
-            # ignore hidden files
-            if f[0] == '.':
-                continue
-            try:
-                image_data = prepare_image(path)
-                image_data.save(f)
-                # train_images = np.append(train_images, np.array([image_data]), axis=0)
-            except OSError:
-                os.remove(path)
+    for f in os.listdir(path):
+        fpath = "{}/{}".format(path, f)
+        # ignore hidden files
+        if f[0] == '.':
+            continue
+        try:
+            image_data = prepare_image(fpath)
+            image_data.save(prepared_path+"/"+f)
+        except OSError:
+            os.remove(fpath)
 
-
-# for as many train_images as there are, set all of their labels to be 1
-# train_labels = np.ones(train_images.shape[0])
-
-# return (train_images, train_labels)
+if __name__ == "__main__":
+    create_dataset(sys.argv[1])
--- tensorflow/train_model.py
+++ tensorflow/train_model.py
@@ -1,9 +1,32 @@
 import tensorflow as tf
 from tensorflow import keras
+from PIL import Image
 import numpy as np
 import os
 
-from prepare_image import prepare_image
+def import_training_data(path):
+    size = (64,64)
+    abspath = os.path.abspath(path)
+
+    # preprocess the image data and compile it for training
+    images = np.empty((0, size[0]*size[1]), float)
+
+    for f in os.listdir(abspath):
+        im = Image.open(abspath +"/" + f)
+
+        # get the numeric data from the image
+        image_data = np.array(list(im.getdata()))
+
+        # divide all the data by 255
+        image_data = np.divide(image_data, 255)
+
+        images = np.append(images, np.array([image_data]), axis=0)
+
+
+    # for as many train_images as there are, set all of their labels to be 1
+    labels = np.ones(images.shape[0])
+    
+    return (images, labels)
 
 def train_model(train_images, train_labels):
     # build the model architecture
@@ -22,8 +45,13 @@ def train_model(train_images, train_labels):
 
     return model
 
-def test_model(test_images, test_labels)
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+def test_model(test_images, test_labels):
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
 
-print('Test accuracy:', test_acc)
+    print('Test accuracy:', test_acc)
 
+if __name__ == "__main__":
+    (images, labels) = import_training_data("./tensorflow/training_data_prepared")
+
+    print(images)
+    print(labels)
