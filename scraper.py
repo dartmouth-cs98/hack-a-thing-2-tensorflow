629b119bb7288e62a73b9ee972dce12e7023605f
ijemmao <ijemmao>
2019-01-14 19:55:59 -0500

    updating

--- scraper.py
+++
@@ -1,7 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-
-arguments = {"Keywords":"Empanadas", "limit":100, "print_urls":False}
-
-absolute_images_paths = response.download(arguments)
