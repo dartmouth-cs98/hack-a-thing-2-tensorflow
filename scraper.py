38df55c6f76f17dde8e9c9d93a0f4ac454a3ccf2
ijemmao <ijemmao>
2019-01-14 19:56:55 -0500

    saving files

---
+++ scraper.py
@@ -0,0 +1,7 @@
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+arguments = {"Keywords":"Empanadas", "limit":100, "print_urls":False}
+
+absolute_images_paths = response.download(arguments)
